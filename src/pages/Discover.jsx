539891505061bcd8e62bc5268fe0805077becc29
rraj-official <119522203+rraj-official@users.noreply.github.com>
2023-07-26 14:08:14 +0530

    First commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,12 @@
+import { Error, Loader, SongCard } from "../components";
+import { genres } from '../assets/constants'
+
+const Discover = () => {
+
+    return (
+        <div className="flex flex-col">Discover</div>
+    );
+    
+};
+
+export default Discover;
